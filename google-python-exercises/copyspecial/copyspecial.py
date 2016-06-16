d4836d910023f538a6c1dc386013498fae85b7b8
jacques <oelofsej@gmail.com>
2016-06-16 16:20:45 +0800

    Completed. Sys utilities

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -14,11 +14,32 @@ import commands
 
 """Copy Special exercise
 """
-
 # +++your code here+++
 # Write functions and modify main() to call them
+def get_special_path(dir):
+  filenames = os.listdir(dir)
+  abspaths = []
+  for file in filenames:
+    match = re.search(r'__(\w+)__', file)
+    if match:
+      abspaths.append(os.path.abspath(os.path.join(dir, file)))
+  print abspaths
+  return abspaths
 
+def copy_to(paths, dir):
+  if not os.path.exists(dir): os.mkdir(dir)
+  for srcpath in paths:
+    fname = os.path.basename(srcpath)
+    shutil.copy(srcpath, os.path.join(dir, fname))
 
+def zip_to(paths, zipfile):
+  zips = ' ' + ' '.join(paths)
+  (status, output) = commands.getstatusoutput('zip -j ' + zipfile + zips)
+
+  # If there is an error status is non-zero. Then write output to std error ,
+  if status:
+    sys.stderr.write(output)
+    sys.exit(1)
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -50,6 +71,11 @@ def main():
 
   # +++your code here+++
   # Call your functions
-  
+  if args[0]: paths = get_special_path(args[0])
+  if todir:
+    copy_to(paths, todir)
+  if tozip:
+    zip_to(paths, tozip)
+
 if __name__ == "__main__":
   main()
